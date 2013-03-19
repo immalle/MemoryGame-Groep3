1d7a74909bb165bd4eb1a48b84718ee8900f3cac
Thomas De Meyer <ThomasDeMeyer07@hotmail.com>
2013-03-19 09:44:29 +0100

    startscherm memory game
    Thomas De Meyer

---
+++ mapmemory/StartSchermMemoryGame/StartSchermMemoryGame/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Blue
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
