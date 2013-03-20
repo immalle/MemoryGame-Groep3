aa09b6c3aae77df8919996a8189675df2f0309a8
Tim Dries <tim.dries@immalle.be>
2013-03-20 12:00:05 +0100

    Pictures tonen op het veld
    Nog niet volledig random

--- MemoryGamePicturebox/MemoryGamePicturebox/Form1.vb
+++ MemoryGamePicturebox/MemoryGamePicturebox/Form1.vb
@@ -3,28 +3,37 @@
 
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        Dim imageList As New List(Of String)
 
-        imageList.Add(My.Resources.ResourceManager.GetObject("Image1"))
-        imageList.Add(My.Resources.ResourceManager.GetObject("Image2"))
-        imageList.Add(My.Resources.ResourceManager.GetObject("Image3"))
+        Dim list As New ArrayList
+
+        list.Add(My.Resources.ResourceManager.GetObject("Image1"))
+        list.Add(My.Resources.ResourceManager.GetObject("Image2"))
+        list.Add(My.Resources.ResourceManager.GetObject("Image3"))
+        list.Add(My.Resources.ResourceManager.GetObject("Image1"))
+        list.Add(My.Resources.ResourceManager.GetObject("Image2"))
+        list.Add(My.Resources.ResourceManager.GetObject("Image3"))
 
 
+        Dim rand As New Random
+        Dim index As Integer
+        Dim item As Object
+
+
+
+        Dim listIndex As Integer = 0
         For i = 0 To 1
             For j = 0 To 2
-                Dim rnd As New Random
-
-                imageList.RemoveAt(1)
-
                 Dim picbox As New PictureBox()
                 picbox.Size = New Size(20, 20)
                 picbox.Location = New Point(30 * i, 30 * j)
 
-                picbox.Image = 
+                picbox.Image = list(listIndex)
+                listIndex += 1
 
                 Me.Controls.Add(picbox)
             Next
         Next
+       
 
 
         'AddHandler picbox.MouseEnter, AddressOf PicboxOnMouseEnterEventHandler
